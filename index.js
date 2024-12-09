571b9ed168751a3c834c39a89d2715ccea52945f
dennis <denismukuria@gmail.com>
2024-12-09 23:21:30 +0300

    add 3 functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+} const mondayWork= function(act="go to the office"){
+    return `This Monday, I will ${act}.`; 
+};
+function wrapAdjective(par="*"){
+    return function(innerPar="special"){
+        return `You are ${par}${innerPar}${par}!`;
+    }
+}
