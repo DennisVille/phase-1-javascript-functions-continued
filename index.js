713d71a8c608630cac861c8b91cf5acbc90beaef
dennis <denismukuria@gmail.com>
2024-12-12 20:26:09 +0300

    undo changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // code your solution here
 function saturdayFun(activity="roller-skate"){
-    return console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`;
 } 
 const mondayWork= function(act="go to the office"){
-    return console.log(`This Monday, I will ${act}.`); 
+    return `This Monday, I will ${act}.`; 
 }
 function wrapAdjective(par="*"){
     return function(innerPar="special"){
-        return console.log(`You are ${par}${innerPar}${par}!`);
+        return `You are ${par}${innerPar}${par}!`;
     }
 }
