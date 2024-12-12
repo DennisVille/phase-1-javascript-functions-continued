6796dbfe3d7acedffce0fb50e5ab50da9a3ed1a0
dennis <denismukuria@gmail.com>
2024-12-12 20:20:38 +0300

    tested the code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // code your solution here
 function saturdayFun(activity="roller-skate"){
-    return `This Saturday, I want to ${activity}!`;
-} const mondayWork= function(act="go to the office"){
-    return `This Monday, I will ${act}.`; 
-};
+    return console.log(`This Saturday, I want to ${activity}!`);
+} 
+const mondayWork= function(act="go to the office"){
+    return console.log(`This Monday, I will ${act}.`); 
+}
 function wrapAdjective(par="*"){
     return function(innerPar="special"){
-        return `You are ${par}${innerPar}${par}!`;
+        return console.log(`You are ${par}${innerPar}${par}!`);
     }
 }
